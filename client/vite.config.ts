269fe533cbd0635bd85416b2a86d2b8261921ea3
Drew Wilson <jankyy93@gmail.com>
2025-03-22 12:08:12 -0400

    Added tailwind and Shadcn

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,15 @@
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import checker from "vite-plugin-checker";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), checker({ typescript: true })],
+  plugins: [react(), checker({ typescript: true }), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
