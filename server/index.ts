775011405b4a0cee084cf91cafad53b33b98b188
Drew Wilson <jankyy93@gmail.com>
2025-03-22 21:47:45 -0400

    linked db to backend. added route files

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,15 @@
+import { Request, Response } from "express";
+
 const express = require("express");
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
 });
